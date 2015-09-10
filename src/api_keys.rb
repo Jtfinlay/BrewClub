6ec0e1359f7606cbce65bba78fdf41a7e0957c81
James Finlay <jtfinlay@ualberta.ca>
2015-09-09 23:04:33 -0700

    Added API keys and Main files
    - API key & secret should be filled with proper client key & secret before run

---
+++ src/api_keys.rb
@@ -0,0 +1,11 @@
+# encoding: utf-8
+#
+# = api_keys.rb
+#
+# API id & secret storage
+#
+# Author: James Finlay
+##
+
+CLIENT_ID = "<put client_id here>"
+CLIENT_SECRET = "<put client_secret here>"
---
+++ src/main.rb
@@ -0,0 +1,12 @@
+# encoding: utf-8
+#
+# = main.rb
+#
+# Main file for running Untappd cataloguer
+#
+# Author: James Finlay
+##
+
+require './api_keys'
+
+// todo: jtfinlay: ...
